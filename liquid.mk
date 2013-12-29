18323b7f301c26ef9d12cd4295caebdd14797f47
Sean Hoyt <seanhoyt963@gmail.com>
2013-12-28 18:55:56 -0600

    Liquify ls980

---
+++ liquid.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2013 The LiquidSmooth Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# boot
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# device
+$(call inherit-product, device/lge/ls980/ls980.mk)
+
+# phone
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# telephony
+$(call inherit-product, vendor/liquid/config/common_cdma.mk)
+
+## product
+PRODUCT_DEVICE := ls980
+PRODUCT_BRAND := LGE
+PRODUCT_NAME := liquid_ls980
+PRODUCT_MODEL := LG-LS980
+PRODUCT_MANUFACTURER := lge
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.ls980.$(shell date +%m%d%y).$(shell date +%H%M%S)
+
+# override
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=LS980ZV7.1378543552 \
+    PRODUCT_NAME=ls980 \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    PRIVATE_BUILD_DESC="g2_spr-user 4.2.2 JDQ39B LS980ZV7.1378543552 release-keys" \
+    BUILD_FINGERPRINT="lge/g2_spr_us/g2:4.2.2/JDQ39B/LS980ZV7.1378543552:user/release-keys"
+
+PRODUCT_PACKAGES += Torch
